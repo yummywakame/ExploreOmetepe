83d3dfdd0e6ecee7883f836f827596e9ce1c6f4c
Olivia Meiring <olivia@yummy-wakame.com>
2018-07-10 23:25:17 -0700

    improved variable handling.

--- app/src/main/java/com/yummywakame/exploreometepe/DetailActivity.java
+++ app/src/main/java/com/yummywakame/exploreometepe/DetailActivity.java
@@ -18,11 +18,9 @@ package com.yummywakame.exploreometepe;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.annotation.IntegerRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
